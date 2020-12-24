d1cb33c903fc78ecb23ce19b8fe14798d824f98d
SinaSystem <sinasystem47@gmail.com>
2020-12-24 20:42:28 +0330

    complete AddHydratedBlocPackage

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,9 +1,10 @@
 import 'package:bloc/bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 import 'counter_state.dart';
 
 
-class CounterCubit extends Cubit<CounterState> {
+class CounterCubit extends HydratedCubit<CounterState>  {
   CounterCubit() : super(CounterState(counterValue: 0));
 
   void increment() => emit(
@@ -11,4 +12,13 @@ class CounterCubit extends Cubit<CounterState> {
 
   void decrement() => emit(CounterState(
       counterValue: state.counterValue - 1));
+
+  @override
+  CounterState fromJson(Map<String, dynamic> json) {
+    return CounterState.fromMap(json);}
+
+  @override
+  Map<String, dynamic> toJson(CounterState state) {
+    return state.toMap();}
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:counter_with_cubit_flutter/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'cubit/counter_cubit.dart';
 import 'package:counter_with_cubit_flutter/cubit/counter_state.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+
+
+void main() async  {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory(),);
+
   runApp(MyApp());
 }
 
